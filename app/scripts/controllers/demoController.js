439c8332dcdeeb82d17c65bca98be140e442be26
NareshSaravanan <manareshsaravanan@gmail.com>
2016-02-22 16:10:38 +0800

    Adding Dist
    Including the build files

--- app/scripts/controllers/demoController.js
+++ app/scripts/controllers/demoController.js
@@ -4,25 +4,44 @@
 angular.module('demoInAngularJsApp')
   .controller('DemoCtrl', function ($scope,getvoyagedata,getCurrencList) {
     $scope.tabs = [
-      { title:'Dynamic Title 1', content:'Dynamic content 1' },
-      { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
+      { title:'Selection',section:'selection' ,active:true},
+      { title:'Registration',section:'registration'}
     ];
-
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.selectionTab = true;
+    $scope.edit = function(){
+      $scope.tabs[1].active = true;
+      $scope.tabSection = 'registration';
+    };
+    $scope.check = function(section){
+      $scope.searchResults = [];
+      $scope.tabSection = section;
+    };
     $scope.getCurrencList = getCurrencList;
     $scope.voyagePlan  = getvoyagedata;
+    $scope.searchResults = [];
+    $scope.searchbtnfn = function(){
+      $scope.searchResults.push($scope.voyagePlan);
+      $scope.gridOptions1.data = $scope.searchResults;
+    };
     $scope.voyagePlan.statisticsPeriod =   new Date($scope.voyagePlan.statisticsPeriod);
     $scope.gridOptions = {
+      enableColumnResizing: true,
+      enableSorting: true
+    };
+    $scope.gridOptions1 = {
       enableColumnResizing: true,
       enableSorting: true
     };
     $scope.gridOptions.columnDefs = [
       {name:"Currency", enableCellEdit: true},
       {name:"ExchangeRate",enableCellEdit: true}
+    ];
+    $scope.gridOptions1.columnDefs = [
+      { name: 'Actions', cellTemplate: '<div class="ui-grid-cell-contents glyphicon glyphicon-pencil" ng-click="grid.appScope.edit(row)"></div>' },
+      {name:"vessel"},
+      {name:"voyage"},
+      {name:"currency"},
+      {name:"leg"}
     ];
     $scope.gridOptions.enableColumnResizing = true;
     $scope.gridOptions.enableFiltering = true;
